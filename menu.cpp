0f0ee4f87257a1119b9757d1f26767320ba2de5f
ilkiyani <ilkiyani@gmail.com>
2022-05-17 16:19:11 -0700

    implemented menu stubs

---
+++ menu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//TODO:Prompt user for input and run matching function
+	return 0;
+}
+void display(){
+	//TODO:display the board
+}
+void move(){
+	//TODO:ask which piece they want to move and move it
+}
+void newgame(){
+	//TODO:start a new game
+}
+void quit(){
+	//TODO:quit the current game and exit the program
+}
+
