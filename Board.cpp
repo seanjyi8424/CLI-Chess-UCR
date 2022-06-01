28b82b705f42a92775abf10600afa030bd14c9af
syi012 <syi012@ucr.edu>
2022-06-01 11:42:49 -0700

    Moved menu function to Board, edited Board functions

--- Board.cpp
+++ Board.cpp
@@ -8,9 +8,58 @@ Board::Board() {
 }
 
 Board::Board(string b) {
+	//name = b;
+	//populate();
 	board[row][column] = b;
 }
-
+void Board::menu() {
+    char input = 0;
+    while (input != 'q' && gameOver == false) {
+        outputGame();
+	    std::cout << "Currently " << getPlayer() << "'s turn..." << std::endl
+	    << "Enter input: "; //MENU OPTIONS: (m)ove, (u)ndo, (s)ave, (l)oad, (n)ew game, (q)quit
+	    std::cin >> input;
+            std::cout << std::endl;
+	    if (input == 'm') {
+		char a;
+		int b;
+		char x;
+		int y;
+		std::cout << "Enter piece to move" << std::endl
+			  << "x axis: " << std::endl;
+		std::cin >> a;
+		std::cout << "y axis: " << std::endl;
+		std::cin >> b;
+		std::cout << "Enter where to move" << std::endl
+			  << "x axis: " << std::endl;
+		std::cin >> x;
+		std::cout << "y axis: " << std::endl;
+		std::cin >> y;
+		move(8 - b, int(tolower(a)) - 97, 8 - y, int(tolower(x)) - 97);
+	    }
+	    else if (input == 's') {
+		save();
+            }
+	    else if (input == 'l') {
+		load();
+	    }
+	    else if (input == 'n') {
+		newGame();
+	    }
+	    else if (input == 'q') { //Added this statement because pressing q results in error message and then closes the loop//
+		std::cout << "Game has ended, goodbye" << std::endl;
+	    }
+	    else { //encourages users to use lower case inputs, upper case will not be recognized// 
+		std::cout << "Invalid input, try again" << std::endl;
+	    }
+    }
+}
+string Board::getPlayer() {
+    if (turn == 0) {
+       return "white";
+    }
+    return "black";
+}
 void Board::emptyBoard() {
 	board[row][column];
 	for (int i = 0; i < row; i++) {
--- Board.h
+++ Board.h
@@ -1,7 +1,7 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include <iostream>
-
+#include <piece.hpp>
 using namespace std;
 
 class Board {
@@ -9,16 +9,18 @@ private:
     int row;
     int column;
     string board[8][8];
+    bool gameOver = false;
+    string name;
+    int turn = 0;
 
 public:
     Board();
-
     Board(string);
-
-    void emptyBoard();
-
+    void menu();
     void printBoard();
-
     void updateBoard();
+    void populate(); //implement later after piece class//
+    void move(int, int, int, int);
+    string getPlayer();
 };
 #endif
